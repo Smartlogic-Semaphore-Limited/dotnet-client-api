250aebbb390572790b159dc2b26e9ffd30843a41
Piotr Nowakowski <piotrn@smartlogic.com>
2020-04-14 11:36:22 +0200

    Migrated back to .NET Framework

---
+++ src/Smartlogic.Semaphore.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Smartlogic Semaphore Api")]
+[assembly: AssemblyDescription("A client library that can be used to connect to Semaphore services from Microsoft .Net framework applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Smartlogic Semaphore Ltd")]
+[assembly: AssemblyProduct("Smartlogic Semaphore .")]
+[assembly: AssemblyCopyright("Copyright © Smartlogic Semaphore Ltd 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9275e3e2-649d-4c48-a8b2-79e65c16a3b7")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.16")]
+[assembly: AssemblyFileVersion("3.7.0.00001")]
